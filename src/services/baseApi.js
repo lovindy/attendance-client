6d4d0e55ea74f05a69101b9c56f9e7b9385ad145
Seang Makaramarady <seangdy085@gmail.com>
2024-09-10 16:32:35 +0700

    fix

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api/v1', // Replace with API URL
+    baseUrl: 'http://44.198.191.149:8000/api/v1',
     headers: (token) => ({
-      Authorization: `Bearer ${token}`, // Add authorization header
+      Authorization: `Bearer ${token}`,
     }),
-    credentials: 'include', // Include cookies in requests
+    credentials: 'include',
   }),
   tagTypes: ['Users', 'Admins', 'Teachers', 'Students'], // Add all possible tag types here
   endpoints: () => ({}), // We’ll extend this in other API slices
